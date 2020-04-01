2a3f3832c87d9b0505a157ae53d176f0c71e4e63
John Ruiz <jruiz@johnruiz.com>
2020-04-01 18:48:21 -0400

    add a html endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from influxdb import InfluxDBClient
 
 
@@ -22,4 +23,34 @@ def get_prices(days: int = 7):
     )
 
     return [data_point for data_point in result_set.get_points()]
-    
+
+@app.get("/", response_class=HTMLResponse)
+def get_index():
+    return """
+    <html>
+    <head>
+    <title>WoW Gold Prices</title>
+    </head>
+    <body>
+      <h2>Recent Gold Prices on Herod - Horde</h2>
+      <canvas id="myChart" width="85%" height="85%"></canvas>
+      <script src="https://cdnjs.com/libraries/Chart.js"></script>
+      <script>
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            const data = this.responseText;
+            const renamed_data = data.map(point => ({'t': point['time'],'y': point['price']}))
+            var ctx = document.getElementById("myChart")
+            var myChart = new Chart(ctx, {
+              type: "line",
+              data: renamed_data
+            })
+          }
+        };
+
+        xhttp.open("GET", "http://compute.ruiz.house:8088/prices", true);
+        xhttp.send();
+      </script>
+    </body>
+    </html>
+    """
